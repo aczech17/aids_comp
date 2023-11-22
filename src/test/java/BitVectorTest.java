8ef50091768edd58a6aede68c93c2afea0fb73ad
aczech17 <adrian-czech@outlook.com>
2023-11-23 00:47:44 +0100

    new bit vector tests

--- src/test/java/BitVectorTest.java
+++ src/test/java/BitVectorTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import Structures.BitVector;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +25,21 @@ public class BitVectorTest
         vector.pushBit(1);
 
         assertEquals(1, vector.getBit(-1));
+    }
+
+    @Test
+    public void put3Assymetric()
+    {
+        BitVector vector = new BitVector();
+
+        vector.pushBit(1);
+        vector.pushBit(1);
+        vector.pushBit(0);
+
+        int[] expectedBits = {1, 1, 0};
+
+        for (int i = 0; i < 3; i++)
+            assertEquals(expectedBits[i], vector.getBit(i));
     }
 
     @Test
@@ -68,13 +85,94 @@ public class BitVectorTest
     {
         BitVector vector = new BitVector();
 
-        vector.pushBit(0);
         vector.pushBit(1);
-
         vector.popBit();
-
         vector.pushBit(0);
 
         assertEquals(0, vector.getBit(-1));
+    }
+
+    @Test
+    public void push0PopItAndThenPush1()
+    {
+        BitVector vector = new BitVector();
+
+        vector.pushBit(0);
+        vector.popBit();
+        vector.pushBit(1);
+
+        assertEquals(1, vector.getBit(-1));
+    }
+
+    @Test
+    public void compareTwoSame()
+    {
+        BitVector vector1 = new BitVector();
+        BitVector vector2 = new BitVector();
+
+        vector1.pushBit(0);
+        vector2.pushBit(0);
+
+        vector1.pushBit(1);
+        vector2.pushBit(1);
+
+        vector1.pushBit(0);
+        vector2.pushBit(0);
+
+        vector1.pushBit(0);
+        vector2.pushBit(0);
+
+        vector1.pushBit(1);
+        vector2.pushBit(1);
+
+        assertEquals(vector1, vector2);
+    }
+
+    @Test
+    public void compareTwoOther()
+    {
+        BitVector vector1 = new BitVector();
+        BitVector vector2 = new BitVector();
+
+        vector1.pushBit(1);
+        vector2.pushBit(1);
+
+        vector1.pushBit(1);
+        vector2.pushBit(0);
+
+        vector1.pushBit(1);
+        vector2.pushBit(1);
+
+        assertNotEquals(vector1, vector2);
+    }
+
+    @Test
+    public void compare3OnesAnd2Ones()
+    {
+        BitVector vector1 = new BitVector();
+        BitVector vector2 = new BitVector();
+
+        vector1.pushBit(1);
+        vector1.pushBit(1);
+
+        vector2.pushBit(1);
+        vector2.pushBit(1);
+        vector2.pushBit(1);
+
+        assertNotEquals(vector1, vector2);
+    }
+
+    @Test
+    public void cloneTest0()
+    {
+        BitVector vector1 = new BitVector();
+
+        vector1.pushBit(1);
+        vector1.pushBit(1);
+        vector1.pushBit(1);
+
+        var vector2 = vector1.clone();
+
+        assertEquals(vector1, vector2);
     }
 }
