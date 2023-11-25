18e3e5753246987d84d0acbfa2beb668c12ea37d
aczech17 <adrian-czech@outlook.com>
2023-11-25 02:19:16 +0100

    testing on files

--- src/main/java/AidsComp.java
+++ src/main/java/AidsComp.java
@@ -1,21 +1,45 @@
 import Structures.HuffmanTree;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
 
 public class AidsComp
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        RandomAccessFile file = new RandomAccessFile("test.txt", "rw");
+        RandomAccessFile input = null;
+        try
+        {
+            input = new RandomAccessFile("data/data.txt", "rw");
+        }
+        catch (FileNotFoundException exception)
+        {
+            System.out.println("file not found :/");
+            return;
+        }
 
-        file.writeByte((int)'A');
-        file.writeByte((int)'B');
-        file.writeByte((int)'C');
+        BitWriter output = null;
+        try
+        {
+            output = new BitWriter("output");
+        }
+        catch (IOException e)
+        {
+            System.out.println("nie utworzono pliku output");
+            return;
+        }
 
-        file.seek(0);
-        file.writeByte((int)'X');
+        try
+        {
+            Compressor.compress(input, output);
+        }
+        catch (IOException exception)
+        {
+            System.out.println(exception.getMessage());
+            return;
+        }
     }
 }
